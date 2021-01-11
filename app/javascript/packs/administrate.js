07992ab6381f0dee66e1f742c6b91914a4baa336
JavaDogDev <JavaDog.Dev@gmail.com>
2021-01-11 00:02:55 -0500

    Completely remove Sprockets

---
+++ app/javascript/packs/administrate.js
@@ -0,0 +1,23 @@
+/* https://github.com/thoughtbot/administrate/wiki/Rails-6-&-Webpacker */
+
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require('@rails/ujs').start()
+require('turbolinks').start()
+
+// The next line you only need if you want ActiveStorage support
+require('@rails/activestorage').start()
+
+// The next line you only need if you need channels in administrate
+require('channels')
+
+// The next two lines you only need if you want ActionText support
+// require('trix')
+// require('@rails/actiontext')
+
+require('../administrate/index')
+
+import '../stylesheets/administrate.scss';
