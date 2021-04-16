037b6365dadaf40292c6abc86933801881d0cc55
JavaDogDev <JavaDog.Dev@gmail.com>
2021-04-15 23:32:53 -0400

    Add selectize plugins remove_button and drag_drop

--- app/javascript/administrate/index.js
+++ app/javascript/administrate/index.js
@@ -1,3 +1,9 @@
+import jQuery from 'jquery';
+import 'jquery-ui';
+import 'jquery-ui/ui/widgets/sortable';
+window.$ = window.jQuery = jQuery;
+
+import 'selectize';
+
 import '../components/table';
 import '../components/date_time_picker';
-import 'selectize';
