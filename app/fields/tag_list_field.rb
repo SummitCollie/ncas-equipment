521e7b6d566c005708725f20eb8ea392ada31198
JavaDogDev <JavaDog.Dev@gmail.com>
2021-04-17 13:30:11 -0400

    Move selectize init out of partial

--- app/fields/tag_list_field.rb
+++ app/fields/tag_list_field.rb
@@ -1,7 +1,19 @@
 require 'administrate/field/base'
 
 class TagListField < Administrate::Field::Base
-  def to_s
-    data
+  def selected_tags
+    data.map do |tag|
+      [tag.name, tag.id]
+    end
+  end
+
+  def default_options
+    (most_used_tags + selected_tags).uniq
+  end
+
+  def most_used_tags
+    ActsAsTaggableOn::Tag.most_used(10).map do |tag|
+      [tag.name, tag.id]
+    end
   end
 end
