4f8bd1426667bde34860c8b1064909f8f4605423
JavaDogDev <JavaDog.Dev@gmail.com>
2021-04-17 23:13:36 -0400

    Add ajax tag search to asset dashboard

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,10 @@
 class TagsController < ApplicationController
   def search
     authorize(:tag, :index?)
-    head(:ok)
+    return head(:bad_request) unless params[:query].present?
+
+    puts params[:query]
+    results = ActsAsTaggableOn::Tag.named_like(params[:query]).limit(50)
+    render(json: results)
   end
 end
