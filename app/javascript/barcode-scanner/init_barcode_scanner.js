810f474d0eb3b016aff349bb7007a4e752e75012
JavaDogDev <JavaDog.Dev@gmail.com>
2021-05-11 01:14:38 -0400

    Init camera and draw very helpful and necessary red line

--- app/javascript/barcode-scanner/init_barcode_scanner.js
+++ app/javascript/barcode-scanner/init_barcode_scanner.js
@@ -1,5 +1,37 @@
+import { BrowserMultiFormatReader } from '@zxing/browser';
+
 const initBarcodeScanner = () => {
-  console.log('ran barcode scanner init');
+  const videoElement = document.getElementById('video-element');
+  // const $cycleCamerasButton = $('#cycle-cameras-button');
+  let currentStream;
+  let cameras = [];
+
+  const constraints = {
+    video: { facingMode: 'environment' },
+    audio: false,
+  };
+
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(stream => {
+      currentStream = stream;
+      videoElement.srcObject = stream;
+
+      $('.starting-message').css('display', 'none');
+      $('.red-bar').css('display', 'block');
+
+      return navigator.mediaDevices.enumerateDevices();
+    })
+    .then(devices => {
+      cameras = devices.filter(device => device.kind === 'videoinput');
+    })
+    .catch(err => console.error("Couldn't access cameras:\n", err));
 };
+
+function stopMediaTracks(stream) {
+  stream.getTracks().forEach(track => {
+    track.stop();
+  });
+}
 
 export default initBarcodeScanner;
