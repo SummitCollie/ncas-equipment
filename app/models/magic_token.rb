335467349e4013ff797be8c42436d218d4282786
JavaDogDev <JavaDog.Dev@gmail.com>
2021-05-15 02:40:04 -0400

    Fix bug in MagicToken hooks

--- app/models/magic_token.rb
+++ app/models/magic_token.rb
@@ -1,8 +1,8 @@
 class MagicToken < ApplicationRecord
   belongs_to :user
 
+  before_validation :assign_default_values, on: :create
   after_save :destroy_expired_tokens
-  after_initialize :assign_default_values
 
   validates_presence_of :token
   validates_presence_of :expires
