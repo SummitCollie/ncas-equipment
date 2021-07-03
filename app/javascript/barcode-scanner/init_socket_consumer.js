9f57e1e0ff7e54175632eceec3a4c014979679e4
JavaDogDev <JavaDog.Dev@gmail.com>
2021-07-03 01:40:01 -0400

    Hook up barcode action -> desktop client flow all the way!
    yaaaaaas
    goddamn this was way too hard

--- app/javascript/barcode-scanner/init_socket_consumer.js
+++ app/javascript/barcode-scanner/init_socket_consumer.js
@@ -18,21 +18,25 @@ const initSocketConsumer = eventHandler => {
       },
 
       received(data) {
-        switch (data.message_type) {
-          case BarcodeApp.message_types.ACTION_CHANGED:
-            eventHandler.emit('action-changed', data.action);
-            break;
+        if (data.target === 'scanner') {
+          switch (data.message_type) {
+            case BarcodeApp.message_types.ACTION_CHANGED:
+              eventHandler.emit('action-changed', data.action);
+              break;
 
-          case BarcodeApp.message_types.ASSET_DATA:
-            eventHandler.emit('got-asset-data', data);
-            break;
+            case BarcodeApp.message_types.ASSET_DATA:
+              eventHandler.emit('got-asset-data', data);
+              break;
 
-          default:
-            console.error(
-              `Got unknown websocket message type '${
-                data.message_type
-              }' containing:\n${JSON.stringify(data, null, 2)}`
-            );
+            default:
+              console.error(
+                `Got unknown websocket message type '${
+                  data.message_type
+                }' containing:\n${JSON.stringify(data, null, 2)}`
+              );
+          }
+        } else if (data.target !== 'desktop') {
+          console.error(`Got message with unknown target '${data.target}'`);
         }
       },
 
@@ -47,10 +51,10 @@ const initSocketConsumer = eventHandler => {
       /**
        * Send a message back to the server that user wants to perform the current action
        * on the current barcode (set barcode on asset, add it to checkout, etc).
-       * The JS on the other end of user's BarcodeWebChannel will deal with it.
+       * The JS on the desktop end of user's BarcodeScannerChannel will deal with it.
        */
-      confirm_action(action, barcode) {
-        this.perform('confirm_action', { action, barcode });
+      confirm_action(actionName, barcode) {
+        this.perform('confirm_action', { actionName, barcode });
       },
     }
   );
