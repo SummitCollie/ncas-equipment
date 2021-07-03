d68eca1787e968b7c4d703d8e5bdd534ed073384
JavaDogDev <JavaDog.Dev@gmail.com>
2021-07-02 22:32:54 -0400

    [BROKEN] Failed attempt at inter-socket messaging. Need to rethink...

--- app/javascript/barcode-scanner/init_socket_consumer.js
+++ app/javascript/barcode-scanner/init_socket_consumer.js
@@ -2,13 +2,18 @@ import { createConsumer } from '@rails/actioncable';
 
 const initSocketConsumer = eventHandler => {
   const socketConsumer = createConsumer();
+  let lastScannedBarcode = null;
 
   return socketConsumer.subscriptions.create(
     { channel: 'BarcodeScannerChannel' },
     {
       connected() {
-        eventHandler.on('barcode-scanned', barcode =>
-          this.barcode_scanned(barcode)
+        eventHandler.on('barcode-scanned', barcode => {
+          lastScannedBarcode = barcode;
+          this.barcode_scanned(barcode);
+        });
+        eventHandler.on('confirm-action', action =>
+          this.confirm_action(action, lastScannedBarcode)
         );
       },
 
@@ -24,13 +29,28 @@ const initSocketConsumer = eventHandler => {
 
           default:
             console.error(
-              `Got unknown websocket message type '${data.message_type}'`
+              `Got unknown websocket message type '${
+                data.message_type
+              }' containing:\n${JSON.stringify(data, null, 2)}`
             );
         }
       },
 
+      /**
+       * A barcode was scanned, send it to the server.
+       * The server will fire another event with result data.
+       */
       barcode_scanned(barcode) {
         this.perform('barcode_scanned', { barcode });
+      },
+
+      /**
+       * Send a message back to the server that user wants to perform the current action
+       * on the current barcode (set barcode on asset, add it to checkout, etc).
+       * The JS on the other end of user's BarcodeWebChannel will deal with it.
+       */
+      confirm_action(action, barcode) {
+        this.perform('confirm_action', { action, barcode });
       },
     }
   );
