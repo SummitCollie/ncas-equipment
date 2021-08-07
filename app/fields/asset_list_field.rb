992fc17da41b3d112aec959206a772436ca3fab5
JavaDogDev <JavaDog.Dev@gmail.com>
2021-08-07 23:24:43 +0200

    Fix asset_list_field default/selected options so they init with colors

--- app/fields/asset_list_field.rb
+++ app/fields/asset_list_field.rb
@@ -4,4 +4,15 @@ class AssetListField < Administrate::Field::Base
   def selected_assets
     data
   end
+
+  def default_options
+    (selected_assets + Asset.order('RANDOM()').first(10)).uniq(&:id)
+      .as_json(methods: :primary_tag_color)
+  end
+
+  private
+
+  def with_primary_tag_colors(assets)
+    assets.map(&:primary_tag_color)
+  end
 end
