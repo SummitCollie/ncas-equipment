c4e78f2733edecd8eec45dca691ed8a7a77186d4
JavaDogDev <JavaDog.Dev@gmail.com>
2021-09-18 00:28:35 -0400

    Limit search on check-out/check-in to checked-in/checked-out assets, respectively

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -5,8 +5,11 @@ class AssetsController < ApplicationController
   def search
     authorize(:asset, :show?)
     return head(:bad_request) unless params[:query].present?
+    return head(:bad_request) if params[:scope].present? && ['can_check_in',
+                                                             'can_check_out',].exclude?(params[:scope])
 
-    assets = Asset.where('name ILIKE ?', "%#{params[:query]}%").order(:name).limit(20)
+    search_scope = params[:scope].present? ? Asset.public_send(params[:scope]) : Asset.all
+    assets = search_scope.where('name ILIKE ?', "%#{params[:query]}%").order(:name).limit(20)
 
     render(json: assets, methods: :primary_tag_color)
   end
