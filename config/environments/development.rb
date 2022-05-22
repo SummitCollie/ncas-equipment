bac086ff1b547a668275f412a1805abb8a4e2f8a
JavaDogDev <JavaDog.Dev@gmail.com>
2022-05-21 20:20:30 -0400

    Downgrade to node 16, SSL thing is annoying

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,10 +60,12 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  # https://github.com/heroku/heroku-buildpack-ruby/issues/576#issuecomment-1013923925
+  # config.assets.debug = true
 
   # Suppress logger output for asset requests.
-  config.assets.quiet = true
+  # https://github.com/heroku/heroku-buildpack-ruby/issues/576#issuecomment-1013923925
+  # config.assets.quiet = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
