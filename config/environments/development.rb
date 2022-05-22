6cd399e81ea032bec0d01c8ddeef04731a0e3cf9
JavaDogDev <JavaDog.Dev@gmail.com>
2022-05-21 20:54:55 -0400

    Fix environment configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,12 +60,10 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  # https://github.com/heroku/heroku-buildpack-ruby/issues/576#issuecomment-1013923925
-  # config.assets.debug = true
+  config.assets.debug = true
 
   # Suppress logger output for asset requests.
-  # https://github.com/heroku/heroku-buildpack-ruby/issues/576#issuecomment-1013923925
-  # config.assets.quiet = true
+  config.assets.quiet = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
